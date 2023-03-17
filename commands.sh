7c63be37cee8efa733f7f2a94982d7b390df1855
aravind-musigumpula <aravind.musigumpula@freshworks.com>
2023-03-17 17:23:37 +0530

    correcting commands.sh

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "creating elastalert indices"
-sh /usr/local/bin/elastalert-create-index --config /data/elastalert/config.yaml --verbose
+python /usr/local/bin/elastalert-create-index --config /data/elastalert/config.yaml --verbose
 echo "Starting elastalert"
-sh /usr/local/bin/elastalert --config /data/elastalert/config.yaml --verbose
+python /usr/local/bin/elastalert --config /data/elastalert/config.yaml --verbose

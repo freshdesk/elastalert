981c3aa6a9d009c38844c3afa5e0d74a8fe236fe
aravind-musigumpula <aravind.musigumpula@freshworks.com>
2023-03-17 16:44:05 +0530

    correcting entrypoint

--- commands.sh
+++ commands.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "creating elastalert indices"
-/usr/local/bin/elastalert-create-index --config /data/elastalert/config.yaml --verbose
+sh /usr/local/bin/elastalert-create-index --config /data/elastalert/config.yaml --verbose
 echo "Starting elastalert"
-/usr/local/bin/elastalert --config /data/elastalert/config.yaml --verbose
+sh /usr/local/bin/elastalert --config /data/elastalert/config.yaml --verbose

5bba03202fe2767721619ea196a8de9db68e0cfe
Naga Vijay Kumar <naga.kumar@freshworks.com>
2025-11-28 16:34:17 +0530

    test

--- elastalert/elastalert.py
+++ elastalert/elastalert.py
@@ -2560,7 +2560,6 @@ def main(args=None):
 
         if not client.args.silence:
             client.start()
-        a = 1/0
     except Exception as e:
         # Global exception handler inside main() - catch ALL unhandled exceptions
         # This prevents the process from crashing and avoids crashloops in K8s
